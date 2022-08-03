611f81220deb29e999f80e91131ed6ece464a587
Alien-Web <surumypallickal@gmail.com>
2022-08-03 17:04:13 +0530

    End of section 2

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+        //Navigatio properties
+        public Guid studentId { get; set; }
+    }
+}
